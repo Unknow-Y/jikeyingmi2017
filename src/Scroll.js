b5711e3db4c4682c1f093827b4c5fd98d867fc70
soyaine <soyaine@126.com>
2018-03-16 19:02:56 +0800

    add timeline

---
+++ src/Scroll.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as ScrollMagic from 'scrollmagic';
+
+class Scroll extends Component {
+  render() {
+    return (
+      <div>xxxx</div>
+    )
+  }
+}
+
+export default Scroll;
